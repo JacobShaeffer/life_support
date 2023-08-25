e79b1f7ec4cb0f50f8e5e1a2ae76c4dd83a67e49
JacobShaeffer <shaeffer.jacob@gmail.com>
2023-08-25 12:30:36 -0700

    add 127.0.0.1 to hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 	config.hosts << "lifesupport.local"
+	config.hosts << "127.0.0.1"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
